b252a11c706cc29d5ee6d66e38060ba2dc45c3c1
Oskars Sylwan <oskars.sylwan@gmail.com>
2017-10-25 19:53:16 +0200

    added item schema and model

---
+++ models/item.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Schemas
+const ItemSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: {type: String, default: 'Lorem Ipsum Description'},
+  origin: {type: String, default: 'USA'},
+  material: {type: String, default: 'Cotton'},
+  color: String,
+  categories: [String],
+});
+
+
+// Models
+const Item = mongoose.model('Item', ItemSchema);
+
+// Exports
+module.exports = Item;
