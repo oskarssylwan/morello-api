27f9f61e4d61c1641a35ee4d01deb75b1f0e0a20
Oskars Sylwan <oskars.sylwan@gmail.com>
2017-10-25 19:53:44 +0200

    added basic functionality

--- routes/items.js
+++ routes/items.js
@@ -3,28 +3,61 @@
 const express = require('express');
 const router = express.Router();
 
+// DB Models
+const Item = require('../models/item');
+
+//Middleware
+router.param('itemID', (req, res, next, id) => {
+  Item.findById(id, (error, item) => {
+    if (error) return next(error);
+    if (!item) return next(new Error('Item could not be found'));
+    req.item = item;
+    return next();
+  });
+});
+
+
 // Get Routes
 router.get('/', (req, res, next) => {
-  res.send('Welcome to /items');
+  if (!req.query.categories) return next(new Error('No category specified'));
+  Item.find({ categories: { $all: req.query.categories.split(',')}}, (error, items) => {
+      if (error) return next(error);
+      if (items.length <= 0) return next(new Error('No items found'));
+      res.json(items);
+    }
+   );
 });
 
 router.get('/:itemID', (req, res, next) => {
-  res.send(`The item you requested is: ${req.params.itemID}`);
+  res.json({message: 'Item retrieved successfully!', item: req.item})
 });
 
 // Post Routes
 router.post('/', (req, res, next) => {
-  res.json(req.body);
+  //Create DB entry for item
+  const item = new Item(req.body);
+  item.save((error, itemEntry) => {
+    if (error) next(error);
+    res.json({message: 'Item created successfully!', item: item});
+  })
 });
 
 //Put Routes
 router.put('/:itemID', (req, res, next) => {
-  res.json(req.body);
+  req.item.set(req.body);
+  req.item.save((error, item) => {
+    if (error) return next(error);
+
+    res.json({message: 'Item updated successfully!', item: item});
+  });
 });
 
 // delete routes
 router.delete('/:itemID', (req, res, next) => {
-  res.json(req.body);
+  req.item.remove(error => {
+    if (error) return next(error);
+    res.json({message: "Item removed"});
+  })
 });
 
 
