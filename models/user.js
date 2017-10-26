6d42e561e5c7933d91528722cdec9602166f1bb4
Oskars Sylwan <oskars.sylwan@gmail.com>
2017-10-26 14:02:06 +0200

    added pre save hash password functionality

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Schemas
 const UserSchema = new mongoose.Schema({
@@ -29,6 +30,46 @@ const UserSchema = new mongoose.Schema({
     default: [{}]
   }
 });
+
+//Static Methods
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email})
+      .exec(function (error, user) {
+        if(error) {
+          return callback(error);
+        } else if (!user) {
+          const err = new Error('User not found');
+          error.status = 401;
+          return callback(error);
+        }
+        bcrypt.compare(password, user.password, function(error, result) {
+          if (result === true) {
+            return callback(null, user);
+          } else {
+            return callback();
+          }
+        });
+      });
+};
+
+//Hooks
+UserSchema.pre('save', function(next) {
+  let user = this;
+  if(user.password) {
+    bcrypt.hash(user.password, 10, function(error, hashed) {
+      if (error) {
+        return next(error);
+      } else {
+        console.log(hashed);
+        user.password = hashed;
+          next();
+      }
+    });
+  } else {
+    next();
+  }
+});
+
 
 
 // Models
