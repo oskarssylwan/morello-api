a6d014b3ec8c2cb8b3a5097f8824abcf2db85418
Oskars Sylwan <oskars.sylwan@gmail.com>
2017-11-18 11:52:32 +0100

    removed line break from description field

--- models/item.js
+++ models/item.js
@@ -7,11 +7,9 @@ const ItemSchema = new mongoose.Schema({
   name: String,
   price: Number,
   description: {type: [String], default: [
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum,
-    quam quis sollicitudin volutpat, mauris purus ullamcorper sapien.`
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum, quam quis sollicitudin volutpat, mauris purus ullamcorper sapien.`
     ,
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum,
-    quam quis sollicitudin volutpat, mauris purus ullamcorper sapien.`
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum, quam quis sollicitudin volutpat, mauris purus ullamcorper sapien.`
   ]},
   origin: {type: String, default: 'USA'},
   material: {type: String, default: 'Cotton'},
