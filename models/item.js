bd80d4bf2f8859365f5a256ba7813a6d56de81b5
Oskars Sylwan <oskars.sylwan@gmail.com>
2017-11-18 11:44:58 +0100

    updated item description type

--- models/item.js
+++ models/item.js
@@ -6,7 +6,13 @@ const mongoose = require('mongoose');
 const ItemSchema = new mongoose.Schema({
   name: String,
   price: Number,
-  description: {type: String, default: 'Lorem Ipsum Description'},
+  description: {type: [String], default: [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum,
+    quam quis sollicitudin volutpat, mauris purus ullamcorper sapien.`
+    ,
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum,
+    quam quis sollicitudin volutpat, mauris purus ullamcorper sapien.`
+  ]},
   origin: {type: String, default: 'USA'},
   material: {type: String, default: 'Cotton'},
   color: String,
