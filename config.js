4455afd737fe095f939722f7fee6290f9675e76e
Oskars Sylwan <oskars.sylwan@gmail.com>
2017-11-20 14:29:23 +0100

    fixed typo

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   port: 8080,
 
   //Databse setup
-  db_location: proccess.env.MONGODB_URI,
+  db_location: process.env.MONGODB_URI,
   payload_limit: '2000kb',
 
   //Password hashing
